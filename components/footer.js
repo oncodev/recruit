353c7b26667cb24a0f7d5be3ba5bf28f32da38d2
oncodev <drlee@oncocross.com>
2022-04-22 13:57:37 +0900

    feat: add components

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,14 @@
 export default class FooterComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <p>footer</p>
+      <div class="footer">
+        <p>사람을 치료하는 여정에 <br/> 온코크로스와 함께 가실분을 모십니다.</p>
+        <div class="openlink">
+          <a href="/">채용 중인 공고 보기</a>
+        </div>
+        <p>채용 관련 문의는 이메일로 문의 주시면 1~3일 이내로 답변을 드립니다. <br/> oncocross@oncocross.com</p>
+        <p>온코크로스 공식 홈페이지에 들어오시면 더 많은 소개를 보실 수 있습니다. <br/> https://www.oncocross.com</p>
+      </div>
     `;
   }
 }
--- components/header.js
+++ components/header.js
@@ -1,10 +1,18 @@
 export default class HeaderComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <a href="/recruit">홈</a>
-      <a href="../pages/company.html">온코크로스는 어떤 회사일까</a>
-      <a href="../pages/work.html">우리가 하는 의미있는 일</a>
-      <a href="../pages/recruit.html">업무의 기회와 따뜻한 복지</a>
+      <div class="header">
+        <div class="header_mobile_menu">메뉴</div>
+        <div class="logo">
+          <!-- <a href="/recruit">홈</a> -->
+          <a href="/">홈</a>
+        </div>
+        <div class="gnb">
+          <a href="../pages/company.html">온코크로스는 어떤 회사일까</a>
+          <a href="../pages/work.html">우리가 하는 의미있는 일</a>
+          <a href="../pages/welfare.html">업무의 기회와 따뜻한 복지</a>
+        </div>
+      </div>
     `;
   }
 }
