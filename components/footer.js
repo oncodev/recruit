23076091e98cfe3582679b5afb36c52be48cd6bc
oncodev <drlee@oncocross.com>
2022-07-29 15:12:17 +0900

    이메일 수정

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,7 @@
 export default class FooterComponent extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <a href="mailto:oncocross@oncocross.com"><b>채용 관련 문의</b>는 <u>oncocross@oncocross.com</u> 메일로 문의 주시면 1~3일 이내로 답변을 받으실 수 있습니다.</a>
+      <a href="mailto:recruit@oncocross.com"><b>채용 관련 문의</b>는 <u>recruit@oncocross.com</u> 메일로 문의 주시면 1~3일 이내로 답변을 받으실 수 있습니다.</a>
       <a href="https://www.oncocross.com">온코크로스 <b>공식 홈페이지</b> <u>https://www.oncocross.com</u> 들어오시면 더 많은 소개를 보실 수 있습니다.</a>
     `;
   }
